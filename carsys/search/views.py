10c5dbc4ecf3365c7a7f72a811f00738fda36e4f
Li Tang <1365371042@qq.com>
2020-11-08 23:55:47 +0800

    实现了分页查询违章

--- carsys/search/views.py
+++ carsys/search/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from search.models import Record
@@ -11,13 +11,27 @@ def show_index(request: HttpRequest) -> HttpResponse:
 
 
 def search_records(request: HttpRequest) -> HttpResponse:
-    queryset = Record.objects.filter(is_deleted=False)
-    carinfo = request.POST.get('carinfo', '').strip().upper()
-    if carinfo:
-        queryset = queryset.filter(
-            Q(car__carno__startswith=carinfo) | Q(car__owner__contains=carinfo)
-        )
-    return render(request, 'index.html', {
-        'records': queryset,
-        'carinfo': carinfo
-    })
+    try:
+        page = int(request.GET.get('page', '1'))
+        page = page if page >= 1 else 1
+        size = int(request.GET.get('size', '5'))
+        size = size if 0 < size <= 50 else 5
+        queryset = Record.objects.filter(is_deleted=False)
+        carinfo = request.POST.get('carinfo', '').strip().upper()
+        if carinfo:
+            queryset = queryset.filter(
+                Q(car__carno__startswith=carinfo) | Q(car__owner__contains=carinfo)
+            )
+        total_page = (queryset.count() -1) // size + 1
+        queryset = queryset.order_by('-makedate')[(page-1) * size: page * size]
+        return render(request, 'index.html', {
+            'records': queryset,
+            'carinfo': carinfo,
+            'new_page': page,
+            'total_page': total_page,
+            'page_size': size,
+            'has_prev': page > 1,
+            'has_next': page < total_page,
+        })
+    except ValueError:
+        return redirect('/')
